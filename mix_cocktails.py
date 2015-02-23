9069ca5ba437d13fa3d93dd070b55c421f52e5bc
Matt Bellis <matthew.bellis@gmail.com>
2015-02-23 14:29:42 -0500

    Modified mix_cocktails to deal with separate neutron and compton contributions as well as simulate the CoGeNT efficiency.

--- mix_cocktails.py
+++ mix_cocktails.py
@@ -1,5 +1,6 @@
 import numpy as np
 import csv
+from cogent_utilities import cogent_efficiency
 
 import sys
 
@@ -53,33 +54,52 @@ def cut_subranges(data,subrange,which_one=0):
 
 nsamples = int(sys.argv[1])
 
-#'''
+'''
 infile_names = ['MC_files/mc_surface_bulk_samples_1M.dat',
                 'MC_files/mc_flat_bulk_samples_1M.dat',
                 'MC_files/mc_lshell_bulk_samples_1M.dat']
-#'''
+'''
+
+bkg_names = ['surface','neutron','compton','lshell']
+tag = '10k'
+infile_names = []
+for b in bkg_names:
+    name = "MC_files/mc_%s_bulk_samples_%s.dat" % (b,tag)
+    infile_names.append(name)
 
 '''
 infile_names = ['MC_files/mc_surface_bulk_samples_10k.dat',
-                'MC_files/mc_flat_bulk_samples_10k.dat',
+                'MC_files/mc_neutron_bulk_samples_10k.dat',
+                'MC_files/mc_compton_bulk_samples_10k.dat',
                 'MC_files/mc_lshell_bulk_samples_10k.dat']
 '''
 
-central_values = [4482, 3140, 975]
+#central_values = [4482, 3140, 975]
+#central_values = [4482, 862, 2287, 975]
+central_values = [5500, 1100, 2900, 1250]
 ranges,subranges,nbins = parameters.fitting_parameters(0)
+
+#data = [tdays,energies,rise_time]
+
+# CoGeNT efficiency parameters
+threshold = 0.345
+sigmoid_sigma = 0.241
+max_val = 0.86786
+
+
 
 infiles = []
 bulk_bkgs = []
 bkgs = []
 nentries = []
+# Read in the data.
 for name in infile_names:
 
     infile = open(name)
-
     bkg = np.loadtxt(infile)
-    bkgs.append(bkg)
-
     nentries.append(len(bkg))
+
+    bkgs.append(bkg)
 
 
 indices = []
@@ -90,26 +110,48 @@ for n in nentries:
 for i in xrange(0,nsamples):
     tot_bkgs = np.array([])
     num_to_grab = []
-    for bkg,index,cv in zip(bkgs,indices,central_values):
+    testname = "MC_files/sample"
+    for bkg,bkg_name,index,cv in zip(bkgs,bkg_names,indices,central_values):
 
         np.random.shuffle(index)
         ng = np.random.poisson(cv)
         new_bkg = bkg[index][0:ng]
 
-        print "Old len: %d" % (len(new_bkg))
+        # Original number
+        testname += "_%s_%d" % (bkg_name,ng)
 
+        #print new_bkg.shape
+
+        # Apply the CoGeNT efficiency.
+        days = new_bkg[:,0]
+        energies = new_bkg[:,1]
+        risetimes = new_bkg[:,2]
+        data = [days,energies,risetimes]
+        data = cogent_efficiency(data,threshold,sigmoid_sigma,max_val)
+        #print type(data)
+        #print new_bkg
+        new_bkg = np.array([data[0],data[1],data[2]])
+        new_bkg = new_bkg.transpose()
+        #print new_bkg.shape
+
+        testname += "_%d" % (len(new_bkg))
+
+        # Cut out the deadtime regions.
         cut_bkg = cut_subranges(new_bkg,subranges[1],0)
 
-        print "cut len: %d" % (len(cut_bkg))
+        testname += "_%d" % (len(cut_bkg))
+
+        #print "cut len: %d" % (len(cut_bkg))
 
         #num_to_grab.append(ng)
-        num_to_grab.append(len(cut_bkg))
+        #num_to_grab.append(len(cut_bkg))
 
         #print cut_bkg
         tot_bkgs = np.append(tot_bkgs,cut_bkg)
         #print tot_bkgs
 
-    testname = "MC_files/sample_surf_%d_%d_flat_%d_%d_lshell_%d_%d_%04d.dat" % (central_values[0],num_to_grab[0],central_values[1],num_to_grab[1],central_values[2],num_to_grab[2],i)
+    #testname = "MC_files/sample_surf_%d_%d_neutrons_%d_%d_comptons_lshell_%d_%d_%04d.dat" % (central_values[0],num_to_grab[0],central_values[1],num_to_grab[1],central_values[2],num_to_grab[2],i)
+    testname += "_%0d.dat" % (i)
     print testname
     #print tot_bkgs.shape
     #print len(tot_bkgs)
