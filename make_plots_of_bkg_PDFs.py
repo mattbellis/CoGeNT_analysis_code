94fb96335ce26c7e76a9efc706367526846c7e5c
Matthew Bellis <matthew.bellis@gmail.com>
2015-08-08 01:06:04 -0400

    Edits to plots of background.

--- make_plots_of_bkg_PDFs.py
+++ make_plots_of_bkg_PDFs.py
@@ -29,63 +29,72 @@ thi = 1238
 E = np.linspace(elo,ehi,xbins) # energy
 t = np.linspace(tlo,thi,ybins) # time
 
+Eylim = [0,0]
+tylim = [0,0]
+
+fmt = None
+Epdf = None
+tpdf = None
+label = None
+tag = None
+
 if sys.argv[2]=="0":
     # Surface events
     tag = "surface"
     label = "%s events" % (tag)
-    plt.figure(figsize=(6,4))
-    pdf  = pdfs.poly(E,[pars['k1_surf'],pars['k2_surf']],elo,ehi) #,efficiency=efficiency)
-    plt.plot(E,pdf,'y-',linewidth=4,label=label)
-    plt.xlabel(r'Energy (keVee)',fontsize=18)
-    plt.ylabel('PDF (arbitrary units)',fontsize=18)
-    plt.xlim(elo,ehi)
-    plt.ylim(0)
-    plt.tight_layout()
-    plt.legend(fontsize=18)
-    name = "Plots/pdf_%s_E.png" % (tag)
-    plt.savefig(name)
-
-    plt.figure(figsize=(6,4))
-    pdf  = pdfs.exp(t,pars['t_surf'],tlo,thi)#,subranges=subranges[1])
-    plt.plot(t,pdf,'y-',linewidth=4,label=label)
-    plt.xlabel(r'Days since 12/3/2009',fontsize=18)
-    plt.ylabel('PDF (arbitrary units)',fontsize=18)
-    plt.xlim(tlo,thi)
-    plt.ylim(0,0.002)
-    plt.tight_layout()
-    plt.legend(fontsize=18)
-    name = "Plots/pdf_%s_t.png" % (tag)
-    plt.savefig(name)
+    fmt='y-'
+    Epdf  = pdfs.poly(E,[pars['k1_surf'],pars['k2_surf']],elo,ehi) #,efficiency=efficiency)
+    tpdf  = pdfs.exp(t,pars['t_surf'],tlo,thi)#,subranges=subranges[1])
+    Eylim[0] = 0;Eylim[1] = None
+    tylim[0] = 0;tylim[1] = 0.002
 
 
 elif sys.argv[2]=="1":
     # Neutron events
     tag = "neutron"
     label = "%s events" % (tag)
-    plt.figure(figsize=(6,4))
-    pdf  =  pdfs.exp(E,pars['flat_neutrons_slope'],elo,ehi)
-    plt.plot(E,pdf,'c-',linewidth=4,label=label)
-    plt.xlabel(r'Energy (keVee)',fontsize=18)
-    plt.ylabel('PDF (arbitrary units)',fontsize=18)
-    plt.xlim(elo,ehi)
-    plt.ylim(0)
-    plt.legend(fontsize=18)
-    plt.tight_layout()
-    name = "Plots/pdf_%s_E.png" % (tag)
-    plt.savefig(name)
+    fmt = 'c-'
+    Epdf  =  pdfs.exp(E,pars['flat_neutrons_slope'],elo,ehi)
+    tpdf  =  pdfs.poly(t,[],tlo,thi)
+    Eylim[0] = 0;Eylim[1] = None
+    tylim[0] = 0;tylim[1] = 0.0014
 
-    plt.figure(figsize=(6,4))
-    pdf  =  pdfs.poly(t,[],tlo,thi)
-    plt.plot(t,pdf,'c-',linewidth=4,label=label)
-    plt.xlabel(r'Days since 12/3/2009',fontsize=18)
-    plt.ylabel('PDF (arbitrary units)',fontsize=18)
-    plt.xlim(tlo,thi)
-    plt.ylim(0,0.002)
-    plt.legend(fontsize=18)
-    plt.tight_layout()
-    name = "Plots/pdf_%s_t.png" % (tag)
-    plt.savefig(name)
+elif sys.argv[2]=="2":
+    # Compton events
+    tag = "Compton"
+    label = "%s events" % (tag)
+    fmt = 'm-'
+    Epdf  =  pdfs.exp(E,pars['e_exp_flat'],elo,ehi)
+    tpdf  =  pdfs.exp(t,pars['t_exp_flat'],tlo,thi)
+    Eylim[0] = 0;Eylim[1] = 0.60
+    tylim[0] = 0;tylim[1] = 0.0015
 
+
+plt.figure(figsize=(6,4))
+plt.plot(E,Epdf,fmt,linewidth=4,label=label)
+plt.xlabel(r'Energy (keVee)',fontsize=18)
+plt.ylabel('PDF (arbitrary units)',fontsize=18)
+plt.xlim(elo,ehi)
+plt.ylim(Eylim[0],Eylim[1])
+plt.xticks(fontsize=14)
+plt.yticks(fontsize=14)
+plt.legend(fontsize=18)
+plt.tight_layout()
+name = "Plots/pdf_%s_E.png" % (tag)
+plt.savefig(name)
+
+plt.figure(figsize=(6,4))
+plt.plot(t,tpdf,fmt,linewidth=4,label=label)
+plt.xlabel(r'Days since 12/3/2009',fontsize=18)
+plt.ylabel('PDF (arbitrary units)',fontsize=18)
+plt.xlim(tlo,thi)
+plt.ylim(tylim[0],tylim[1])
+plt.xticks(fontsize=14)
+plt.yticks(fontsize=14)
+plt.legend(fontsize=18)
+plt.tight_layout()
+name = "Plots/pdf_%s_t.png" % (tag)
+plt.savefig(name)
 
 
 plt.show()
